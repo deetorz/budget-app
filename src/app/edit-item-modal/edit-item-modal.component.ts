9df59ec05092a1f4c91121721b40b791ee187cd4
Derek Torres <derek.n.torres@gmail.com>
2022-01-22 01:19:30 +0900

    material install

--- src/app/edit-item-modal/edit-item-modal.component.ts
+++ src/app/edit-item-modal/edit-item-modal.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { BudgetItem } from 'src/shared/models/budget-item.model';
 
 @Component({
   selector: 'app-edit-item-modal',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-item-modal.component.scss']
 })
 export class EditItemModalComponent implements OnInit {
+  @Input() item: BudgetItem;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSubmitted(updatedItem: BudgetItem) {
+
+  }
 }
