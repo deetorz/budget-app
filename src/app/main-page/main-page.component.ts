ed0c12523ddd543e34177e6ad9f8afe10ea787cd
Derek Torres <derek.n.torres@gmail.com>
2022-05-19 01:47:09 +0900

    total budget functionality complete

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -4,23 +4,31 @@ import { BudgetItem } from 'src/shared/models/budget-item.model';
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.scss']
+  styleUrls: ['./main-page.component.scss'],
 })
-
 export class MainPageComponent implements OnInit {
   budgetItems: BudgetItem[] = new Array<BudgetItem>();
+  totalBudget = 0;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   addItem(newItem: BudgetItem) {
     this.budgetItems.push(newItem);
+    this.calculateTotal();
   }
 
   deleteItem(item: BudgetItem) {
-    let index = this.budgetItems.indexOf(item)
+    const index = this.budgetItems.indexOf(item);
     this.budgetItems.splice(index, 1);
+    this.calculateTotal();
+  }
+
+  calculateTotal() {
+    this.totalBudget = 0;
+    this.budgetItems.forEach((item: BudgetItem) => {
+      this.totalBudget += item.amount;
+    });
   }
 }
